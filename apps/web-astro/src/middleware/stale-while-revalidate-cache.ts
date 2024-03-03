2bc9740d4504ded8a9dc17096c28730892c8f617
jok <jok@kruso.dk>
2024-03-03 12:33:03 +0100

    feature: adds server timings

--- apps/web-astro/src/middleware/stale-while-revalidate-cache.ts
+++ apps/web-astro/src/middleware/stale-while-revalidate-cache.ts
@@ -1,19 +1,36 @@
+import type { APIContext, MiddlewareNext } from "astro";
 import { defineMiddleware } from "astro:middleware";
-import type {APIContext, MiddlewareNext} from "astro";
+import { Timer } from "../timer.ts";
 
 export const staleWhileRevalidateCache = defineMiddleware(async (context, next) => {
-  const swr = context.locals.swr ?? 5
-  console.log({ swr })
-  console.log(context.locals.runtime)
-  if (!context.locals.runtime?.env || !swr) return await next();
+  const swr = context.locals.swr ?? 5;
+  const timer = new Timer();
 
+  if (!context.locals.runtime?.env || !swr) return await next();
   const { KV_SWR } = context.locals.runtime.env;
 
-  const cached = await KV_SWR.get<{ response: string; expires: number; }>(context.url.pathname, { type: "json" });
+  timer.time("kvg");
 
-  if(!cached){
+  const cached = await KV_SWR.get<{ response: string; expires: number }>(
+    context.url.pathname,
+    { type: "json" }
+  );
+
+  timer.timeEnd("kvg");
+
+  if (!cached) {
+    timer.time("kvp");
     await put(next, context, KV_SWR, swr);
-    return await next();
+    timer.time("kvp");
+    const res = await next();
+    res.headers.set(
+      "Server-Timing",
+      Object.entries(timer.allTimes()).reduce(
+        (acc, [key, value]) => `${acc}, ${key};dur=${value.value}`,
+        ""
+      )
+    );
+    return res;
   }
 
   const cachedRes = new Response(new TextEncoder().encode(cached.response));
@@ -27,15 +44,21 @@ export const staleWhileRevalidateCache = defineMiddleware(async (context, next) 
   return cachedRes;
 });
 
-async function put(next: MiddlewareNext, context: APIContext<Record<string, any>>, kv: KVNamespace, swr: number) {
+async function put(
+  next: MiddlewareNext,
+  context: APIContext<Record<string, any>>,
+  kv: KVNamespace,
+  swr: number
+) {
   const res = await next();
   const buffer = await res.arrayBuffer();
-  const body = new TextDecoder('utf-8').decode(buffer ?? undefined);
+  const body = new TextDecoder("utf-8").decode(buffer ?? undefined);
 
   await kv.put(
-    context.url.pathname,JSON.stringify({
-        response: body,
-        expires: Date.now() + swr * 1000,
-      }
-    ));
+    context.url.pathname,
+    JSON.stringify({
+      response: body,
+      expires: Date.now() + swr * 1000,
+    })
+  );
 }
---
+++ apps/web-astro/src/timer.ts
@@ -0,0 +1,40 @@
+export class Timer {
+  private readonly _times: Map<
+    string,
+    { key: string; start: [number, number]; value?: number }
+  >;
+
+  constructor() {
+    this._times = new Map();
+  }
+
+  time(name: string) {
+    this._times.set(name, {
+      key: name,
+      start: process.hrtime(),
+    });
+  }
+
+  timeEnd(name: string) {
+    const timeObj = this._times.get(name);
+    if (!timeObj) {
+      return console.warn(`No such name ${name}`);
+    }
+    const duration = process.hrtime(timeObj.start);
+    timeObj.value = duration[0] * 1e3 + duration[1] * 1e-6;
+    this._times.delete(name);
+    return timeObj;
+  }
+
+  clear() {
+    this._times.clear();
+  }
+
+  allTimes() {
+    return this._times;
+  }
+
+  keys() {
+    return this._times.keys();
+  }
+}
