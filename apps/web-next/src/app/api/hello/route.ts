809df3f8971b616f127c2b34316e2b6b1eb112bf
jok <jok@kruso.dk>
2024-04-23 08:50:37 +0200

    chore: test

--- apps/web-next/src/app/api/hello/route.ts
+++ apps/web-next/src/app/api/hello/route.ts
@@ -13,7 +13,7 @@ export async function GET(request: NextRequest) {
   // )
   //
   // KV Example:
-  const cache = getRequestContext().env.KV_SWR;
+  const cache = getRequestContext().env.KV_WEB_NEXT;
   console.log(await cache.getWithMetadata("/swr", { type: "arrayBuffer" }));
   // await myKv.put('suffix', ' from a KV store!')
   // const suffix = await myKv.get('suffix')
