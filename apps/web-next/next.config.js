191cafcc0364755fbc8b428bdf4bf98c764d8acd
jok <jok@kruso.dk>
2024-05-08 16:34:41 +0200

    feat: cache header next

--- apps/web-next/next.config.js
+++ apps/web-next/next.config.js
@@ -1,8 +1,6 @@
 // Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
 // (when running the application with `next dev`), for more information see:
 // https://github.com/cloudflare/next-on-pages/blob/5712c57ea7/internal-packages/next-dev/README.md
-import { Buffer } from "node:buffer";
-
 if (process.env.NODE_ENV === "development") {
   const { setupDevPlatform } = require("@cloudflare/next-on-pages/next-dev");
   setupDevPlatform();
@@ -21,14 +19,14 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  async headers() {
+  headers: async () => {
     return [
       {
         source: "/swr",
         headers: [
           {
-            key: "x-custom-header",
-            value: "my custom header value",
+            key: "x-cache-custom",
+            value: "cache 10",
           },
         ],
       },
